5b206e688d35b89f0b5c15b4db4245088114f6c2
romes <rodrigo.m.mesquita@gmail.com>
2022-08-02 16:12:01 +0200

    Revert ad-hoc union find rebuilding

--- src/Data/Equality/Graph/ReprUnionFind.hs
+++ src/Data/Equality/Graph/ReprUnionFind.hs
@@ -10,7 +10,6 @@ module Data.Equality.Graph.ReprUnionFind where
 
 import Data.Equality.Graph.Classes.Id
 import qualified Data.Equality.Utils.IntToIntMap as IIM
-import qualified Data.IntMap as IM
 
 import GHC.Exts ((+#), Int(..), Int#)
 
@@ -21,11 +20,11 @@ import GHC.Exts ((+#), Int(..), Int#)
 -- e.g. @FUF $ fromList [(y, Canonical), (x, Represented y)]@
 data ReprUnionFind = RUF !IIM.IntToIntMap -- ^ Map every id to either 0# (meaning its the representative) or to another int# (meaning its represented by some other id)
                          RUFSize -- ^ Counter for new ids
-                         !(IM.IntMap [ClassId]) -- ^ Mapping from an id to all its children: This is used for "rebuilding" (compress all paths) when merging. Its a hashcons?
+                         -- !(IM.IntMap [ClassId]) -- ^ Mapping from an id to all its children: This is used for "rebuilding" (compress all paths) when merging. Its a hashcons?
                          -- [ClassId] -- ^ Ids that can be safely deleted after the e-graph is rebuilt
 
 instance Show ReprUnionFind where
-  show (RUF _ s _) = "RUF with size:" <> show (I# s)
+  show (RUF _ s) = "RUF with size:" <> show (I# s)
 
 type RUFSize = Int#
 
@@ -45,13 +44,13 @@ newtype Repr
 emptyUF :: ReprUnionFind
 emptyUF = RUF IIM.Nil
               1# -- Must start with 1# since 0# means "Canonical"
-              mempty
+              -- mempty
               -- mempty
 
 -- | Create a new e-class id in the given 'ReprUnionFind'.
 makeNewSet :: ReprUnionFind
            -> (ClassId, ReprUnionFind) -- ^ Newly created e-class id and updated 'ReprUnionFind'
-makeNewSet (RUF im si hc) = ((I# si), RUF (IIM.insert si 0# im) ((si +# 1#)) (IM.insert (I# si) mempty hc))
+makeNewSet (RUF im si) = ((I# si), RUF (IIM.insert si 0# im) ((si +# 1#)))
 {-# SCC makeNewSet #-}
 
 -- | Union operation of the union find.
@@ -59,18 +58,18 @@ makeNewSet (RUF im si hc) = ((I# si), RUF (IIM.insert si 0# im) ((si +# 1#)) (IM
 -- Given two leader ids, unions the two eclasses making @a@ the leader.(that is,
 -- @b@ is represented by @a@
 unionSets :: ClassId -> ClassId -> ReprUnionFind -> (ClassId, ReprUnionFind)
-unionSets a@(I# a#) b@(I# b#) (RUF im si hc) = (a, RUF new_im si new_hc)
-  where
-    represented_by_b = hc IM.! b
-    -- Overwrite previous id of b (which should be 0#) with new representative (a)
-    -- AND "rebuild" all nodes represented by b by making them represented directly by a
-    new_im = {-# SCC "rebuild_im" #-} IIM.unliftedFoldr (\(I# x) -> IIM.insert x a#) (IIM.insert b# a# im) represented_by_b
-    new_hc = {-# SCC "adjust_hc" #-} IM.adjust ((b:) . (represented_by_b <>)) a (IM.delete b hc)
+unionSets a@(I# a#) (I# b#) (RUF im si) = (a, RUF (IIM.insert b# a# im) si)
+  -- where
+    -- represented_by_b = hc IM.! b
+    -- -- Overwrite previous id of b (which should be 0#) with new representative (a)
+    -- -- AND "rebuild" all nodes represented by b by making them represented directly by a
+    -- new_im = {-# SCC "rebuild_im" #-} IIM.unliftedFoldr (\(I# x) -> IIM.insert x a#) (IIM.insert b# a# im) represented_by_b
+    -- new_hc = {-# SCC "adjust_hc" #-} IM.adjust ((b:) . (represented_by_b <>)) a (IM.delete b hc)
 {-# SCC unionSets #-}
 
 -- | Find the canonical representation of an e-class id
 findRepr :: ClassId -> ReprUnionFind -> ClassId
-findRepr (I# v0) (RUF m' _ _) =
+findRepr (I# v0) (RUF m' _) =
   let
     go :: IIM.IntToIntMap -> Int# -> Int#
     go m v =
@@ -78,7 +77,6 @@ findRepr (I# v0) (RUF m' _ _) =
         0# -> v        -- v is Canonical (0# means canonical)
         x  -> go m x   -- v is Represented by x
    in I# (go m' v0)
-
 -- ROMES:TODO: Path compression in immutable data structure? Is it worth
 -- the copy + threading?
 --
@@ -88,6 +86,11 @@ findRepr (I# v0) (RUF m' _ _) =
 -- When using the ad-hoc path compression in `unionSets`, the depth of
 -- recursion never even goes above 1!
 {-# SCC findRepr #-}
+
+
+-- {-# RULES
+--    "union/find" forall a b x im. findRepr (I# b) (RUF (IIM.insert b a im) x) = I# a
+--   #-}
 
 -- -- | Delete nodes that have been merged after e-graph has been rebuilt
 -- rebuildUF :: ReprUnionFind -> ReprUnionFind
